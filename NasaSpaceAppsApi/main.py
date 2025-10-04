96a45cc5d07d0905e19c070ba4ae405c08938c62
Nightdragon <crackytrini@gmail.com>
2025-10-04 11:08:32 -0600

    hkjldsf
    jadsñlfkj

--- NasaSpaceAppsApi/main.py
+++ NasaSpaceAppsApi/main.py
@@ -1,13 +1,15 @@
 from fastapi import FastAPI
-
+import uvicorn
+import sqlalchemy
 app = FastAPI()
+
 
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
 
-
+#para iniciarlo  uvicorn main:app --reload
 @app.get("/hello/{name}")
 async def say_hello(name: str):
     return {"message": f"Hello {name}"}
