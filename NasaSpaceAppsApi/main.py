2882124c7f411250874e2f334628a3540e6e2c04
Nightdragon <crackytrini@gmail.com>
2025-10-04 12:10:21 -0600

    cxc<x
    fadsfasdfsa

--- NasaSpaceAppsApi/main.py
+++ NasaSpaceAppsApi/main.py
@@ -1,6 +1,15 @@
 from fastapi import FastAPI
+import csv
 
+from ClasesApi.EntradaDatos import EntradaDatos
+from ClasesApi.Resultados import Reultados
 app = FastAPI()
+
+with open("SB_publication_PMC.csv") as file:
+    reader = csv.reader(file)
+    header = next(reader)
+    data_rows = [row for row in header]
+
 
 
 @app.get("/")
@@ -11,5 +20,15 @@ async def root():
 @app.get("/hello/{name}")
 async def say_hello(name: str):
     return {"message": f"Hello {name}"}
+
+
+@app.get("/prueba")
+async def prueba():
+    palabras = []
+    palabras.append("holla")
+    palabras.append("hola")
+    palabras.append("cola")
+    p = Reultados(palabras)
+    return p
 
 
