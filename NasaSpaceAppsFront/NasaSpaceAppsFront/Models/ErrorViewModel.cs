b65962ada6e76a92fbce3668ff63c103d4ba621a
Nightdragon <crackytrini@gmail.com>
2025-10-04 10:21:48 -0600

    inicio
    ajsldñkjfs

---
+++ NasaSpaceAppsFront/NasaSpaceAppsFront/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace NasaSpaceAppsFront.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
